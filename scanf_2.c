acf2f32fbfeafea723e7212c7f2e13bcca9bbbc7
frac25 <you@example.comsylvain.dubois25@hotmail.fr>
2025-04-22 10:10:40 +0200

    Vtraining

--- scanf_2.c
+++ scanf_2.c
@@ -4,29 +4,33 @@
 #include<stdarg.h>
 #include <ctype.h>
 
-
 // autorise : isspace isigit fgetc ungetc ferror feof stdin va_list va_arg va_start va_end va_copy
 
 int scan_int(int fd, va_list ap)
 {
+	int *val;
 	char buf[1];
-	int* var;
-	int sign = 1;
+	int signe = 1;
 
-	var = va_arg(ap, int*);
-	*var = 0;
+	printf("passe int\n");
+
+	val = va_arg(ap, int*);
+	val[0] = 0;
 
 	read(fd, buf, 1);
-	while (buf[0] != '\0' && isspace(buf[0]))
-		read(fd, buf, 1);
-	if(buf[0] == '-')
+
+	while(buf[0] && isspace(buf[0]))
 	{
-		sign = -1;
 		read(fd, buf, 1);
 	}
-	while(isdigit(buf[0]))
+	if (buf[0] == '-')
 	{
-		*var = 10 * var[0] + buf[0]- '0';
+		signe = -1;
+		read(fd, buf, 1);
+	}
+	while(buf[0] && isdigit(buf[0]))
+	{
+		val[0] = val[0] * 10 + buf[0] - '0';
 		read(fd, buf, 1);
 	}
 	return(1);
@@ -65,6 +69,8 @@ int scan_string(int fd, va_list ap)
 	return(1);
 }
 
+
+
 int receiv_input(const char format, va_list ap)
 {
 	int fd = 0;
@@ -74,10 +80,14 @@ int receiv_input(const char format, va_list ap)
 		case 'c' :
 			return scan_char(fd, ap);
 		case 'd':
+//			skip_input_space();
 			return scan_int(fd, ap);
 		case 's' :
+//			skip_input_space();
 			return scan_string(fd, ap);
 	}
+
+
 	return(0);
 }
 
@@ -100,34 +110,17 @@ int ft_scanf(const char *format, ...)
 		}
 		i++;
 	}
-	va_end(ap);
 	return(result);
-
+	va_end(ap);
 }
 
 int main(void)
 {
 	int d;
 	char c;
-	char c2;
-	char s[100];
-	int r = 0;
+	char *s;
 
-//	write(2, "entrez c : \n", 12);
-//	r = scanf("%c", &c);
-//	printf("c = %c   r = %d\n", c, r);
-
-//	write(2, "entrez d : \n", 12);
-//	r = scanf("%d", &d);
-//	printf("d = %d   r = %d\n", d, r);
-
-//	write(2, "entrez s : \n", 12);
-//	r = scanf("%s", s);
-//	printf("s = %s   r = %d\n", s, r);
-
-
-	write(2, "entrez c d s c2 : \n", 19);
-	r = ft_scanf("%c %d %s %c", &c, &d, s, &c2);
-	printf("c = %c   d = %d   s = %s   c2 = %c   r = %d\n", c, d, s, c2, r);
-
+	printf(" entrez d");
+	ft_scanf("%d", &d);
+	printf("d = %d", d);
 }
